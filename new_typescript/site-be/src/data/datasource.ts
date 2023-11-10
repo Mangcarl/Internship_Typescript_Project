c6e6bf4d18c641a2b64f1eaf2e48cddd152c3414
rcaramol-trajector <ryan.caramol@trajector.com>
2023-11-11 05:04:56 +0800

    typescript
    asd

---
+++ new_typescript/site-be/src/data/datasource.ts
@@ -0,0 +1,16 @@
+const sampleData = [
+    {
+        "id": 1,
+        "body": "First Item",
+        "severity": 1,
+        "status": 0
+    },
+    {
+        "id": 2,
+        "body": "Second Item",
+        "severity": 2,
+        "status": 1
+    }
+  ]
+  
+  export default sampleData;
